461af2654b13bc20c757a70f2bc067430b812cf6
haughar <haughar@uw.edu>
2016-02-05 13:45:49 -0800

    Add notifications to dashboard.

--- skilltreeapp/views/pages.py
+++ skilltreeapp/views/pages.py
@@ -5,12 +5,74 @@ from django.contrib.auth import authenticate, login, logout
 from django.core.context_processors import csrf
 from skilltreeapp.forms import LoginForm
 from django.shortcuts import render_to_response, HttpResponseRedirect
+import datetime
+from labgeeks_hermes.models import Notification
+from labgeeks_hermes.forms import NotificationForm
 
 # Create your views here.
 
 def home(request):
     params = {}
-    return render_to_response('pages/home.html', params, context_instance=RequestContext(request))
+    c = {}
+    c.update(csrf(request))
+    if request.user.is_authenticated():
+        # hermes code goes here
+        locations = request.user.location_set.all()
+
+        now = datetime.datetime.now()
+
+        notifications = Notification.objects.all()
+        events = []
+        alerts = []
+        for noti in notifications:
+            if noti.due_date:
+                if now.date() - noti.due_date.date() >= datetime.timedelta(days=1):
+                    noti.archived = True
+                elif not noti.due_date.date() - now.date() > datetime.timedelta(days=7) and not noti.archived:
+                    events.append(noti)
+            else:
+                if not noti.archived:
+                    alerts.append(noti)
+        events.sort(key=lambda x: x.due_date)
+
+        form_is_valid = True
+        if request.method == 'POST':
+            archive_ids = request.POST.getlist('pk')
+            if archive_ids:
+                for archive_id in archive_ids:
+                    notif = Notification.objects.get(pk=archive_id)
+                    notif.archived = True
+                    notif.save()
+                return HttpResponseRedirect('/')
+
+            form = NotificationForm(request.POST)
+            if form.is_valid():
+                form_is_valid = True
+                notification = form.save(commit=False)
+                notification.user = request.user
+                if notification.due_date:
+                    if now.date() - notification.due_date.date() >= datetime.timedelta(days=1):
+                        notification.archived = True
+                notification.save()
+                return HttpResponseRedirect('/')
+            else:
+                form_is_valid = False
+        else:
+            form = NotificationForm()
+
+        params = {
+            'request': request,
+            'events': events,
+            'alerts': alerts,
+            'form': form,
+            'c': c,
+        }
+
+        return render_to_response('pages/home.html', params, context_instance=RequestContext(request))
+    else:
+        form = LoginForm()
+        params = { 'form': form }
+        return HttpResponseRedirect('/accounts/login')
 
 def basic(request):
     params = {}
